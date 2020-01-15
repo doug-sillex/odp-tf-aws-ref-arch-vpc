0b6d59a5c85fdab9fd3d5ef43f262e809f3186ed
RobertJLupinek <9854828608@GSA.GOV>
2020-01-15 09:31:13 -0500

    Adding logic for building out VPC.

--- example/variables.tf
+++ example/variables.tf
@@ -1,18 +1,43 @@
+variable "aws_region" {
+  type = string
+  description = "Default region used by some modules"
+  default = "us-east-1"
+}
+
+variable "project" {
+  type = string
+  description = "Project name"
+  default = "vpc-example"
+}
+
+
+variable "appenv" {
+  type = string
+  description = "Environment for deployment."
+  default = "test"
+
+}
+
 variable "default_cidr" {
   type = string
   description = "Default subnet in CIDR notation."
   default = "10.0.0.0/16"
 }
 
+
 variable "subnets" {
   type = map
   description "Map of subnets"
-  default =  {
+  default =  {}
 }    
 
 variable "nat_gateways" {
-  type = map
   decription "Map of nat gateways"
   default = {}
+  }
 }
+   
 
+variable "route_tables" {
+  default = {}
+}   
