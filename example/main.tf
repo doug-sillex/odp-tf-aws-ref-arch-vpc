1e75f8dfec4b110572e3448e6e216e13c2c91397
RobertJLupinek <9854828608@GSA.GOV>
2020-01-17 09:40:56 -0500

    Addition - adding output variables and subnet CIDR blocks as input.

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,6 @@
 
 provider "aws" {
-  region     = var.aws_region
+  region     = "${var.aws_region}"
 }
 
 module "vpc" {
@@ -10,6 +10,12 @@ module "vpc" {
   vpc_name = "${var.project_name}-${var.appenv}"
   default_cidr = "10.0.0.0/16"
   fisma_id = "${var.fisma_id}"
-
+  default_cidr = "${var.default_cidr}"
+  private_1_a  = "${var.private_1_a}" 
+  private_1_b  = "${var.private_1_b}" 
+  private_2_a  = "${var.private_2_a}" 
+  private_2_b  = "${var.private_2_b}" 
+  public_1_a   = "${var.public_1_a}"  
+  public_1_b   = "${var.public_1_b}"   
 }
 
---
+++ output.tf
@@ -0,0 +1,5 @@
+output "aws_vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
+
+
