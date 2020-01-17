2643eaeb3f2c49d83c0a3e8de9db7e4b5541e22e
RobertJLupinek <9854828608@GSA.GOV>
2020-01-17 10:57:44 -0500

    Addition - adding output variables and subnet CIDR blocks as input.

--- output.tf
+++ output.tf
@@ -3,3 +3,25 @@ output "aws_vpc_id" {
 }
 
 
+output "private_1_a_id" {
+  value = "${aws_subnet.private_1_a.id}"
+}
+
+output "private_2_a_id" {
+  value = "${aws_subnet.private_2_a.id}"
+}
+
+output "private_1_b_id" {
+  value = "${aws_subnet.private_1_b.id}"
+}
+
+output "private_2_b_id" {
+  value = "${aws_subnet.private_2_b.id}"
+}
+
+output "public_1_a_id" {
+  value = "${aws_subnet.public_1_a.id}"
+}
+output "public_1_b_id" {
+  value = "${aws_subnet.public_1_b.id}"
+}
